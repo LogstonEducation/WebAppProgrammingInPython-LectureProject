d417b76ba7f39e14666d37ca092908865a00f99e
Paul Logston <paul.logston@gmail.com>
2024-01-09 16:47:07 -0800

    Add dotenv

--- LectureProject/settings.py
+++ LectureProject/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# Pull env vars from file and place in process environment.
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'django-insecure-&x2*ftz!@ton=*6lp5=50+aja6x$t4&2hp^(7-6@y441qy&(9&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', '') or '').lower() == 'true'
 
 ALLOWED_HOSTS = [
-    'localhost:8000',
+    'localhost',
     'webappprogramminginpython-lectureproject.onrender.com',
 ]
 
