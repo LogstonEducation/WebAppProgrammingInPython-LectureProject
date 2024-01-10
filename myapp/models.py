8e435c8e71e60594d641f43b3e992a5f6b4787d1
Paul Logston <paul.logston@gmail.com>
2024-01-10 08:56:56 -0800

    Add data model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    iso = models.CharField(max_length=3)
+    long_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.iso} {self.long_name}'
+
+
+class Holding(models.Model):
+    currency = models.ForeignKey(
+        Currency,
+        # When the related Currency is deleted, delete this Holding as well.
+        on_delete=models.CASCADE,
+    )
+    value = models.FloatField(default=0.0)
+    buy_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.currency.iso}  {self.value} {self.buy_date}'
+
+
+class Rates(models.Model):
+    currency_one = models.ForeignKey(
+        Currency,
+        # When the related Currency is deleted, delete this Rate as well.
+        on_delete=models.CASCADE,
+        related_name='currency_one_rate_set',
+    )
+    currency_two = models.ForeignKey(
+        Currency,
+        # When the related Currency is deleted, delete this Rate as well.
+        on_delete=models.CASCADE,
+        related_name='currency_two_rate_set',
+    )
+    rate = models.FloatField(default=1.0)
+    last_update_time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.currency_one.iso} {self.currency_two.iso} {self.rate}'
